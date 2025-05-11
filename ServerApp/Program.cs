e1f8e2debf00033a22d43bd69105b63da5ef4640
Capri1545 <chancecapri@hotmail.com>
2025-05-11 10:17:18 -0700

    Update CORS policy to allow any origin for improved flexibility

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy
-            .WithOrigins("http://localhost:5252") // Allow client origin
+            .AllowAnyOrigin() // Allow client origin
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
